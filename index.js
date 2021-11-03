f5b010e5596cd81f4300f315ef03a50500c8634b
uzma <ubkhan3@gmail.com>
2021-11-03 14:47:38 +0000

    complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 //write your code here
+function reverseString(str){
+    let reverse = "";
+    for(let i = str.length-1 ; i >= 0; i--){
+    reverse += str[i];
+  }
+    return reverse;
+}
+  console.log(reverseString('hello'));
